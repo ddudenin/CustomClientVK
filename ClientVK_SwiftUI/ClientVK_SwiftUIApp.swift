0689f7a985275a5fbcf5f5824746eeb5fedb4b6d
Dmitry <dmitry_dudenin@mail.ru>
2021-11-30 19:32:30 +0300

    init commit

---
+++ ClientVK_SwiftUI/ClientVK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientVK_SwiftUIApp.swift
+//  ClientVK_SwiftUI
+//
+//  Created by Дмитрий Дуденин on 30.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ClientVK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
